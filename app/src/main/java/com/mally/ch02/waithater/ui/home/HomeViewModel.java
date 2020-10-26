dc604ede279b0ad7faaf03b84030569bd922bb72
LichRich <leech135789@gmail.com>
2020-10-26 22:05:14 +0900

    base create

---
+++ app/src/main/java/com/mally/ch02/waithater/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.mally.ch02.waithater.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
