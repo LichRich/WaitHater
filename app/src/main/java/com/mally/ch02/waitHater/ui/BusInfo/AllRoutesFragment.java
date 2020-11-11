1cfa9ec41d48d295c8211b13a1911126b14a0cd7
LichRich <leech135789@gmail.com>
2020-11-11 23:25:47 +0900

    예제 기반으로 api데이터 받아오기 구현중...

--- app/src/main/java/com/mally/ch02/waitHater/ui/BusInfo/AllRoutesFragment.java
+++ app/src/main/java/com/mally/ch02/waitHater/ui/BusInfo/AllRoutesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,16 +14,31 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mally.ch02.waitHater.R;
+import com.mally.ch02.waitHater.ui.utils.ListItem;
 
 public class AllRoutesFragment extends Fragment {
+
+//    TAG
+    private final String TAG = "ALL_ROUTES_FRAGMENT";
+
+//    Layout Components
+    private EditText et_routes;
+
+//    API
+    private final String url_main = "http://openapitraffic.daejeon.go.kr/api/rest";
+    private final String api_busRouteInfo = "/busRouteInfo", api_stationInfo = "/stationinfo", api_busPosInfo = "/busposinfo", api_arrive = "/arrive";
+    private final String[] route_operations = {"/getStaionByRoute", "/getStaionByRouteAll", "/getRouteInfo", "/getRouteInfoAll"};
+
+    private ListItem bus;
 
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_all_routes, container, false);
+
+
+
         return root;
     }
 }
